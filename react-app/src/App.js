8c7cec6433be3faf141a2ce54657263441984bd4
Nitin Nair <nitin.dreamern89@gmail.com>
2023-09-04 21:49:47 +0530

    Removed bulma import

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component, lazy, Suspense } from 'react';
-import 'bulma/css/bulma.css';
 import './styles.scss';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import { withRouter } from 'react-router';
@@ -7,7 +6,7 @@ import { HeaderBar, NavBar, NotFound } from './components';
 import About from './About';
 
 const Products = withRouter(
-  lazy(() => import(/* webpackChunkName: "products" */ './products/Products'))
+  lazy(() => import(/* webpackChunkName: "products" */ './products/Products')),
 );
 
 class App extends Component {
